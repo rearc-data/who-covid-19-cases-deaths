7cb10aab89b57c4e1af6415f7a2c5184c2a57619
Norbert Funke <norbert@funke.com>
2021-06-22 10:26:47 -0500

    migrated to data platform

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -5,6 +5,7 @@ import csv
 from html.parser import HTMLParser
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
+from s3_md5_compare import md5_compare
 
 class MyHTMLParser(HTMLParser):
 
@@ -18,20 +19,28 @@ class MyHTMLParser(HTMLParser):
 				if item[0].lower() == 'href' and item[1].endswith('csv'):
 					self.data = item[1]
 
-def source_dataset(new_filename, s3_bucket, new_s3_key):
+def source_dataset(): #(new_filename, s3_bucket, new_s3_key):
 			
 	source_url = 'https://data.humdata.org/dataset/coronavirus-covid-19-cases-and-deaths'
-	
+
+	data_set_name = os.environ['DATASET_NAME']
+	filename = data_set_name
+	new_s3_key = data_set_name + '/dataset/' # + filename
+	s3_bucket = os.environ['ASSET_BUCKET']
+
+	file_location = '/tmp/' + filename
+
 	try:
 		source_response = urlopen(source_url)
 
 	except HTTPError as e:
-		raise Exception('HTTPError: ', e.code, new_filename)
+		raise Exception('HTTPError: ', e.code, filename)
 
 	except URLError as e:
-		raise Exception('URLError: ', e.reason, new_filename)
+		raise Exception('URLError: ', e.reason, filename)
 
 	else:
+
 
 		html = source_response.read().decode()
 		parser = MyHTMLParser()
@@ -43,10 +52,10 @@ def source_dataset(new_filename, s3_bucket, new_s3_key):
 			data_response = urlopen(data_url)
 
 		except HTTPError as e:
-			raise Exception('HTTPError: ', e.code, new_filename)
+			raise Exception('HTTPError: ', e.code, filename)
 
 		except URLError as e:
-			raise Exception('URLError: ', e.reason, new_filename)
+			raise Exception('URLError: ', e.reason, filename)
 
 		else:
 			data = data_response.read()
@@ -54,7 +63,7 @@ def source_dataset(new_filename, s3_bucket, new_s3_key):
 			with open('/tmp/source.csv', 'wb') as s:
 				s.write(data)
 
-			with open('/tmp/source.csv', 'r') as s, open('/tmp/' + new_filename + '.csv', 'w', encoding='utf-8') as c:
+			with open('/tmp/source.csv', 'r') as s, open('/tmp/' + filename + '.csv', 'w', encoding='utf-8') as c:
 				reader = csv.DictReader(s)
 				writer = csv.DictWriter(c, fieldnames=reader.fieldnames)
 				writer.writeheader()
@@ -62,25 +71,37 @@ def source_dataset(new_filename, s3_bucket, new_s3_key):
 				for row in reader:
 					writer.writerow(row)
 
-			with open('/tmp/source.csv', 'r') as s, open('/tmp/' + new_filename + '.json', 'w', encoding='utf-8') as j:
+			with open('/tmp/source.csv', 'r') as s, open('/tmp/' + filename + '.json', 'w', encoding='utf-8') as j:
 				reader = csv.DictReader(s)
 				j.write('\n'.join(json.dumps(row, ensure_ascii=False)
 								  for row in reader))
 			
 			os.remove('/tmp/source.csv')
 
-			asset_list = []
-
 			# Creates S3 connection
 			s3 = boto3.client('s3')
 
+			asset_list = []
+
 			# Looping through filenames, uploading to S3
 			for filename in os.listdir('/tmp'):
+				if filename.startswith(data_set_name):
+					#s3.upload_file('/tmp/' + filename, s3_bucket,
+					#		new_s3_key + filename)
+					file_location = '/tmp/' + filename
+					has_changes = md5_compare(s3, s3_bucket, new_s3_key + filename, file_location)
+					if has_changes:
+						s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
+						asset_list.append(
+							{'Bucket': s3_bucket, 'Key': new_s3_key + filename})
+						print('Uploaded: ' + filename)
+					else:
+						print('No changes in: ' + filename)
 
-				s3.upload_file('/tmp/' + filename, s3_bucket,
-							new_s3_key + filename)
 
-				asset_list.append(
-					{'Bucket': s3_bucket, 'Key': new_s3_key + filename})
 
 			return asset_list
+
+if __name__ == '__main__':
+	asset_list = source_dataset()
+	print(asset_list)
---
+++ run.sh
@@ -0,0 +1,45 @@
+REARC_DATA_PLATFORM_ROLE_ARN='arn:aws:iam::412981388937:role/CrossAccountRole-796406704065-796406704065'
+REARC_DATA_PLATFORM_EXTERNAL_ID='Rearc-Data-Platform-796406704065'
+ASSET_BUCKET='rearc-data-provider'
+MANIFEST_BUCKET='rearc-control-plane-manifest'
+CUSTOMER_ID='796406704065'
+DATASET_NAME='who-covid-19-cases-deaths'
+DATASET_ARN='arn:aws:dataexchange:us-east-1::data-sets/b001062f891df066d421f7a8842a6349'
+PRODUCT_NAME='Coronavirus (COVID-19) Cases and Deaths | World Health Organization, WHO'
+PRODUCT_ID='prod-hwuk2inrwwaca'
+SCHEDULE_CRON="cron(0 4 * * ? *)"
+REGION='us-east-1'
+PROFILE='guardian-pg'
+
+echo "------------------------------------------------------------------------------"
+echo "RearcDataPlatformRoleArn: $REARC_DATA_PLATFORM_ROLE_ARN"
+echo "RearcDataPlatformExternalId: $REARC_DATA_PLATFORM_EXTERNAL_ID"
+echo "CustomerId: $CUSTOMER_ID"
+echo "AssetBucket: $ASSET_BUCKET"
+echo "ManifestBucket: $MANIFEST_BUCKET"
+echo "DataSetName: $DATASET_NAME"
+echo "DataSetArn: $DATASET_ARN"
+echo "ProductName: $PRODUCT_NAME"
+echo "ProductID: $PRODUCT_ID"
+echo "ScheduleCron: $SCHEDULE_CRON"
+echo "Region: $REGION"
+echo "PROFILE: $PROFILE"
+echo "------------------------------------------------------------------------------"
+
+
+# python pre-processing/pre-processing-code/source_data.py
+
+./init.sh \
+    --rdp-role-arn "${REARC_DATA_PLATFORM_ROLE_ARN}" \
+    --rdp-external-id "${REARC_DATA_PLATFORM_EXTERNAL_ID}" \
+    --customer-id "${CUSTOMER_ID}" \
+    --schedule-cron "${SCHEDULE_CRON}" \
+    --asset-bucket "${ASSET_BUCKET}" \
+    --manifest-bucket "${MANIFEST_BUCKET}" \
+    --dataset-name "${DATASET_NAME}" \
+    --product-name "${PRODUCT_NAME}" \
+    --product-id "${PRODUCT_ID}" \
+    --dataset-arn "${DATASET_ARN}" \
+    --region "${REGION}" \
+    --first-revision "false" \
+    --profile "${PROFILE}"
